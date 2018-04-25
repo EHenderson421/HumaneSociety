6c6d17cb4285f2b4de27d1db61d421ab41799872
ehenderson421 <ehenderson421@gmail.com>
2018-04-25 08:19:33 -0500

    add crud opperation for employee queries

--- HumaneSociety/Query.cs
+++ HumaneSociety/Query.cs
@@ -8,7 +8,35 @@ namespace HumaneSociety
 {
     public static class Query
     {
-        
+        public static void RunEmployeeQueries(Employee employee, string crud)
+        {
+            PerformCrudOperationOnEmployee performCrudDelegate;
+            switch (crud)
+            {
+                case "create":
+                    performCrudDelegate = CreateEmployee;
+                    performCrudDelegate(employee, crud);
+                    break;
+
+                case "delete":
+                    performCrudDelegate = DeleteEmployee;
+                    performCrudDelegate(employee, crud);
+                    break;
+
+                case "read":
+                    performCrudDelegate = ReadEmployee;
+                    performCrudDelegate(employee, crud);
+                    break;
+
+                case "update":
+                    performCrudDelegate = UpdateEmployee;
+                    performCrudDelegate(employee, crud);
+                    break;
+
+                default:
+                    break;
+            }
+        }
         
       
     }
